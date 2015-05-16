6d61eb74c409e0d081fdecab7467dabd44432f11
Hubert Nguyen <hubertanguyen@gmail.com>
2015-05-16 14:37:44 -0500

    Add user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   root 'static_pages#new'
+
+  get 'signup' => 'users#new'
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
