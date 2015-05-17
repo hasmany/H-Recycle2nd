d6e699d0c5ff5c740c229ea68cdb9d01f4cdf5a3
Hubert Nguyen <hubertanguyen@gmail.com>
2015-05-17 09:28:34 -0500

    Fix issue where chart would not load due to rails 4 tturbolinks

--- app/assets/javascripts/charts/chartjs-demo.js
+++ app/assets/javascripts/charts/chartjs-demo.js
@@ -1,100 +1,105 @@
-$(function () {
+var ready = function() {
+    $(function () {
+        var lineData = {
+          labels: ["January", "February", "March", "April", "May", "June", "July","August","September","October","November","December"],
+            datasets: [
+                {
+                    label: "Paper",
+                    fillColor: "rgba(163,225,212,0.5)",
+                    strokeColor: "rgba(163,225,212,.7)",
+                    pointColor: "rgba(163,225,212,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(163,225,212,1)",
+                    data: [65, 59, 80, 81, 56, 55, 40,12,56,92,23,67]
+                },
+                {
+                    label: "Plastic",
+                    fillColor: "rgba(35,198,200,0.5)",
+                    strokeColor: "rgba(35,198,200,0.7)",
+                    pointColor: "rgba(35,198,200,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(35,198,200,1)",
+                    data: [28, 48, 40, 19, 86, 27, 90,17,29,43,76,14]
+                },
+                {
+                    label: "Glass/Metal",
+                    fillColor: "rgba(28,132,198,0.5)",
+                    strokeColor: "rgba(28,132,198,0.7)",
+                    pointColor: "rgba(28,132,198,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(28,132,198,1)",
+                    data: [18, 28, 72, 34, 56, 94, 7,56,13,89,36,45]
+                }
+            ]
+        };
 
-    var lineData = {
-        labels: ["January", "February", "March", "April", "May", "June", "July","August","September","October","November","December"],
-        datasets: [
-            {
-                label: "Paper",
-                fillColor: "rgba(163,225,212,0.5)",
-                strokeColor: "rgba(163,225,212,.7)",
-                pointColor: "rgba(163,225,212,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(163,225,212,1)",
-                data: [65, 59, 80, 81, 56, 55, 40,12,56,92,23,67]
-            },
-            {
-                label: "Plastic",
-                fillColor: "rgba(35,198,200,0.5)",
-                strokeColor: "rgba(35,198,200,0.7)",
-                pointColor: "rgba(35,198,200,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(35,198,200,1)",
-                data: [28, 48, 40, 19, 86, 27, 90,17,29,43,76,14]
-            },
-            {
-                label: "Glass/Metal",
-                fillColor: "rgba(28,132,198,0.5)",
-                strokeColor: "rgba(28,132,198,0.7)",
-                pointColor: "rgba(28,132,198,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(28,132,198,1)",
-                data: [18, 28, 72, 34, 56, 94, 7,56,13,89,36,45]
+
+            var lineOptions = {
+                scaleShowGridLines: true,
+                scaleGridLineColor: "rgba(0,0,0,.05)",
+                scaleGridLineWidth: 1,
+                bezierCurve: true,
+                bezierCurveTension: 0.4,
+                pointDot: true,
+                pointDotRadius: 4,
+                pointDotStrokeWidth: 1,
+                pointHitDetectionRadius: 20,
+                datasetStroke: true,
+                datasetStrokeWidth: 2,
+                datasetFill: true,
+                responsive: true,
+            };
+
+            if (document.getElementById('lineChart')) {
+                var ctx = document.getElementById("lineChart").getContext("2d");
+                var myNewChart = new Chart(ctx).Line(lineData, lineOptions);
             }
-        ]
-    };
 
+            var doughnutData = [
+                {
+                    value: parseInt($($('.paperText')[0]).text()) || 1e-1 ,
+                    color: "#a3e1d4",
+                    highlight: "#1ab394",
+                    label: "Paper"
+                },
+                {
+                    value: parseInt($($('.plasticText')[0]).text()) || 1e-1,
+                    color: "#23c6c8",
+                    highlight: "#1ab394",
+                    label: "Plastic"
+                },
+                {
+                    value: parseInt($($('.glassText')[0]).text()) || 1e-1,
+                    color: "#1c84c6",
+                    highlight: "#1ab394",
+                    label: "Glass/Metal"
+                }
+            ];
 
-    var lineOptions = {
-        scaleShowGridLines: true,
-        scaleGridLineColor: "rgba(0,0,0,.05)",
-        scaleGridLineWidth: 1,
-        bezierCurve: true,
-        bezierCurveTension: 0.4,
-        pointDot: true,
-        pointDotRadius: 4,
-        pointDotStrokeWidth: 1,
-        pointHitDetectionRadius: 20,
-        datasetStroke: true,
-        datasetStrokeWidth: 2,
-        datasetFill: true,
-        responsive: true,
-    };
+            var doughnutOptions = {
+                segmentShowStroke: true,
+                segmentStrokeColor: "#fff",
+                segmentStrokeWidth: 2,
+                percentageInnerCutout: 45, // This is 0 for Pie charts
+                animationSteps: 100,
+                animationEasing: "easeOutBounce",
+                animateRotate: true,
+                animateScale: false,
+                responsive: true,
+            };
 
-    if (document.getElementById('lineChart')) {
-        var ctx = document.getElementById("lineChart").getContext("2d");
-        var myNewChart = new Chart(ctx).Line(lineData, lineOptions);
-    }
+            if (document.getElementById('doughnutChart')) {
+                var ctx = document.getElementById("doughnutChart").getContext("2d");
+                var myNewChart = new Chart(ctx).Doughnut(doughnutData, doughnutOptions);
+                window.myDoughnutChart = myNewChart;
+            }
 
-    var doughnutData = [
-        {
-            value: parseInt($($('.paperText')[0]).text()) || 1e-1 ,
-            color: "#a3e1d4",
-            highlight: "#1ab394",
-            label: "Paper"
-        },
-        {
-            value: parseInt($($('.plasticText')[0]).text()) || 1e-1,
-            color: "#23c6c8",
-            highlight: "#1ab394",
-            label: "Plastic"
-        },
-        {
-            value: parseInt($($('.glassText')[0]).text()) || 1e-1,
-            color: "#1c84c6",
-            highlight: "#1ab394",
-            label: "Glass/Metal"
-        }
-    ];
+        });
+};
 
-    var doughnutOptions = {
-        segmentShowStroke: true,
-        segmentStrokeColor: "#fff",
-        segmentStrokeWidth: 2,
-        percentageInnerCutout: 45, // This is 0 for Pie charts
-        animationSteps: 100,
-        animationEasing: "easeOutBounce",
-        animateRotate: true,
-        animateScale: false,
-        responsive: true,
-    };
-
-    if (document.getElementById('doughnutChart')) {
-        var ctx = document.getElementById("doughnutChart").getContext("2d");
-        var myNewChart = new Chart(ctx).Doughnut(doughnutData, doughnutOptions);
-        window.myDoughnutChart = myNewChart;
-    }
-
-});
+$(document).ready(ready);
+// Added on page load, for Rails turbolinks
+$(document).on('page:load', ready);
--- app/assets/javascripts/profile/profile.js
+++ app/assets/javascripts/profile/profile.js
@@ -4,7 +4,12 @@ $(document).ready(function(){
   var recycleButton = $('.saveRecycle');
   // Event Handler for chart animation and recycle creation
   recycleButton.on('click', function() {
-  // Ajax call
+    // Input validation
+    if (!parseInt($('#ionrange_1').attr('value') ) || $('.chosen-select option:selected').text() === "Select") {
+     alert('Please select an amount and/or what you recycled.');
+     return false;
+    }
+    // Ajax call
     $.ajax('/recyclables',{
       type: "POST",
       data: { recycle_type: $('.chosen-select option:selected').text(),
@@ -12,26 +17,38 @@ $(document).ready(function(){
         recycle_date: $('.recycle_date').val(),
         user_id: document.location.pathname.split('/')[2]
       },
-  // On success
+    // On success
       success: function(data) {
         // Set doughnutChart and text value by type
         if (data.recycle_type === "Paper") {
           myDoughnutChart.segments[0].value += data.amount;
-          $('.paperText span').fadeOut(1000, function() {
-            $('.paperText span').html(parseInt($('.paperText span').html()) + data.amount);
-            $('.paperText span').fadeIn(1000);
+          $('.paperText.desktop span').fadeOut(1000, function() {
+            $('.paperText.desktop span').html(parseInt($('.paperText.desktop span').html()) + data.amount);
+            $('.paperText.desktop  span').fadeIn(1000);
+          })
+          $('.paperText.mobile span').fadeOut(1000, function() {
+            $('.paperText.mobile span').html(parseInt($('.paperText.mobile span').html()) + data.amount);
+            $('.paperText.mobile  span').fadeIn(1000);
           })
         } else if (data.recycle_type === "Plastic") {
           myDoughnutChart.segments[1].value += data.amount;
-          $('.plasticText span').fadeOut(1000, function() {
-            $('.plasticText span').html(parseInt($('.plasticText span').html()) + data.amount);
-            $('.plasticText span').fadeIn(1000);
+          $('.plasticText.desktop span').fadeOut(1000, function() {
+            $('.plasticText.desktop span').html(parseInt($('.plasticText.desktop  span').html()) + data.amount);
+            $('.plasticText.desktop span').fadeIn(1000);
+          });
+           $('.plasticText.mobile span').fadeOut(1000, function() {
+            $('.plasticText.mobile span').html(parseInt($('.plasticText.mobile  span').html()) + data.amount);
+            $('.plasticText.mobile span').fadeIn(1000);
           });
         } else {
-          $('.glassText span').fadeOut(1000, function() {
-            $('.glassText span').html(parseInt($('.glassText span').html()) + data.amount);
-            $('.glassText span').fadeIn(1000);
-          })
+          $('.glassText.desktop span').fadeOut(1000, function() {
+            $('.glassText.desktop span').html(parseInt($('.glassText.desktop span').html()) + data.amount);
+            $('.glassText.desktop span').fadeIn(1000);
+          });
+          $('.glassText.mobile span').fadeOut(1000, function() {
+            $('.glassText.mobile span').html(parseInt($('.glassText.mobile span').html()) + data.amount);
+            $('.glassText.mobile span').fadeIn(1000);
+          });
           myDoughnutChart.segments[2].value +=data.amount;
         }
 
